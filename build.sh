e76c460334f9890b1e9e45fdd843736177b77fa4
Takeda-senpai <deadboltsx303@gmail.com>
2023-01-10 11:28:45 +0700

    Add CI

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-set -veuo pipefail
-
 tmp_dir="$(mktemp --tmpdir -d modulebuild.XXXXXXXXXX)"
-function cleanup() {
+cleanup() {
     rm -fr "$tmp_dir"
 }
 trap cleanup EXIT
@@ -30,6 +28,5 @@ unzip "$src_dir/riru/out/safetynet-fix-"*.zip
 unzip "$src_dir/java_module/app/build/outputs/apk/release/app-release.apk" classes.dex
 sha256sum classes.dex | cut -d' ' -f1 | tr -d '\n' > classes.dex.sha256sum
 
-version="$(grep '^version=' module.prop  | cut -d= -f2)"
-rm -f "$src_dir/safetynet-fix-$version.zip"
-zip -r9 "$src_dir/safetynet-fix-$version.zip" .
+rm -f "$src_dir/safetynet-fix.zip"
+zip -r9 "$src_dir/safetynet-fix.zip" .
